1e1dc204a6034d55306fdcbd48635ee450678a68
Anna Lednyova <ashtart@gmail.com>
2023-07-27 23:55:20 +0300

    UserList handleSearch ELSE

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -25,36 +25,22 @@ class UserList extends React.Component {
         : users.map((user) => <UserCard user={user} key={user.login.uuid} />)
     }
 
-    handleSearch = ({target: {value}}) => {
-        //1 Якщо в інпуту нічого немає, то чистимо масив відфільтрованих юзерів
-        // НЕ РАБОТАЕТ
-        if(value === '') {
+    handleSearch = ({ target: { value } }) => {
+        if (value === '') {
             this.setState({
                 filteredUsers: []
-            })
+            });
+        } else {
+            const { users } = this.state;
+            const searchValue = value;
+            const filteredUsers = users.filter((user) =>
+                user.name.last.toLowerCase().trim().indexOf(searchValue.toLowerCase().trim()) !== -1
+            );
+            this.setState({
+                filteredUsers
+            });
         }
-        
-
-        //2 Фільтруємо по прізвищу
-        const {users} = this.state;
-
-        const searchValue = value;
-        const filteredUsers = users.filter((user) => user.name.last.toLowerCase().trim().indexOf(searchValue.toLowerCase().trim()) !== -1
-            // логічний оператор замість if вище так само повертає тру або фолс
-            // if(user.name.last.toLowerCase().trim().indexOf(searchValue.toLowerCase().trim()) !== -1) {
-            //     return true
-            // } else {
-            //     return false
-            //     }
-        ) 
-        
-        //3 Кладемо в стейт відфільтрованих юзерів
-        this.setState({
-            //filteredUsers: filteredUsers
-            // =
-            filteredUsers
-        })
-    }
+    };
 
     setUserCount = ({target: {value}}) => {
         this.setState({
--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ToDoList from "./ToDoList/ToDoList";
+import UserList from "./UserList/UserList";
 
 function App() {
 
     return(
-        <ToDoList />
+        <UserList />
     )
 }
 
