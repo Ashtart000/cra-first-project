5bb9efd469e3ca3cb40099716663059c8e380d72
Anna Lednyova <ashtart@gmail.com>
2023-07-30 00:51:12 +0300

    phones tv DataProvider Render props pattern

---
+++ src/components/DataProvider/DataProvider.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class DataProvider extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            data: [],
+            isLoading: true,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        this.load();
+    }
+    
+    load = () => {
+        //this.props.loadData()
+        this.props.children[0]()
+        .then((data) => {
+            this.setState({
+                data
+            })
+        })
+        .catch((error) => {
+            this.setState({
+                error
+            })
+        })
+        .finally(() => {
+            this.setState({
+                isLoading: false
+            })
+        })
+    }
+
+    render() {
+        //return this.props.renderData(this.state);
+        return this.props.children[1](this.state);
+    }
+}
+
+export default DataProvider;
--- src/components/app.js
+++ src/components/app.js
@@ -1,48 +1,37 @@
 import React from "react";
-import ToDoList from "./ToDoList/ToDoList";
-import UserList from "./UserList/UserList";
-import CounterPage from "./Counter/CounterPage";
-import AlohaDashboard from "./AlohaDashboard/AlohaDashboard"
-//import List from "./List/List";
-//import FlexContainer from "./FlexContainer/FlexContainer";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+//import TVLoader from "./TVLoader/TVLoader";
+//import PhonesLoader from "./PhonesLoader/PhonesLoader";
+import DataProvider from "./DataProvider/DataProvider";
+
 
 function App() {
 
     return(
-        <BrowserRouter>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Go home</Link></li>
-                    <li><Link to='users'>Go to users list</Link></li>
-                    <li><Link to='todo'>Go to todo list</Link></li>
-                    <li><Link to='counter'>Go to counter page</Link></li>
-                    <li><Link to='aloha'>Go to aloha</Link></li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route index element={<Home />}></Route>
-                <Route path="/users" element={<UserList />} />
-                <Route path="/todo" element={<ToDoList />} />
-                <Route path="/counter" element={<CounterPage />} />
-                <Route path="/aloha" element={<AlohaDashboard />} />
-                <Route path="/*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <DataProvider>
+            {() => {
+                return fetch('./tv.json')
+                .then((response) => response.json())
+            }} 
+            {(state) => {
+                const {data, isLoading, error} = state;
+                return (
+                    <>
+                    {isLoading && <div>Loading...</div>}
+                    {error && <div>{error.message}</div>}
+                    <ol>
+                        {data.map((data) => 
+                            <li key={`${Date.now()} ${data.brand} ${data.model}`}>{data.brand} - {data.model} - {data.price}</li>
+                        )}
+                    </ol>
+                    </>
+                );
+            }}
+            </DataProvider>    
+        </>
     )
 }
 
 export default App;
 
-const Home = () => {
-    return <h1>Home page</h1>
-}
 
-const NotFound = () => {
-    return (
-        <>
-            <h1>404</h1>
-            <p>Page not found</p>
-        </>
-    )
-}
