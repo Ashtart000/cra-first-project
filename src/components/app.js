a78d908fbb0e08cff58f626609936d01d8809f9c
Anna Lednyova <ashtart@gmail.com>
2023-08-02 02:02:02 +0300

    setState callback (new state based on previous)

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import React from "react";
-import NewCounterFunction from "./NewCounter/NewCounterFunction";
+import MouseTracker from "./MouseTracker/MouseTracker";
 
 
 class App extends React.Component {
 
     render() {
         return (
-            <NewCounterFunction />
+            <MouseTracker />
         )
     }
 }
