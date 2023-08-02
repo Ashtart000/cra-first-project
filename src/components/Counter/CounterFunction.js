711a28cbfc054c6ea1af46fd5fb5fc0db6ddb893
Anna Lednyova <ashtart@gmail.com>
2023-08-02 19:21:13 +0300

    Counter to TIMER

--- src/components/Counter/CounterFunction.js
+++ src/components/Counter/CounterFunction.js
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
+import { format, addSeconds } from "date-fns";
 
 const CounterFunction = () => {
-    const [count, setCount] = useState(0);
+    const [time, setTime] = useState(new Date(0,0,0,0,0,0,0));
+    const [isRunning, setIsRunning] = useState(true);
+    
+    let intervalId = null;
  
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCount(count => count + 1);
-        }, 1000)
+        if(isRunning) {
+            intervalId = setInterval(() => {
+                setTime(time => addSeconds(time, 1));
+            }, 1000)
+        }
 
         return () => {
             clearInterval(intervalId)
         }
-    }, [])
+    }, [isRunning])
+
+    const switchRunning = () => {
+        setIsRunning(!isRunning);
+    }
 
         return (
             <>
-                <h1>{count}</h1>
+            <h1>{format(time, 'HH:mm:ss')}</h1>
+            <button onClick={switchRunning}>{isRunning ? 'Stop' : 'Start'}</button>
             </>
         )
 }
